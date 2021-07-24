be5019e7f77e79b162aeb07829326d2f54c93285
DCYPHERR <harkiratsinghmkkar@yahoo.com>
2021-07-24 23:42:22 +0530

    24=07

---
+++ movieFestival.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define ar array
+using namespace std;
+const int mxN = 2e5;
+int n;
+int main()
+{       
+    cin >> n;
+    for (int i = 0; i < n; ++i)
+    {
+        int a, b;
+        cin >> a >> b;
+        
+    }
+
+    return 0;
+}
